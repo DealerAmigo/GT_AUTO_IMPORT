22737c95a2f066fa956fca781e393e19c69c4ef9
landing+chatbot-leadmagnet <willquisnos@gmail.com>
2026-06-28 11:28:57 -0400

    Remove Vercel analytics injection
    Removed Vercel analytics injection from the main file.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,2 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { inject } from '@vercel/analytics'
-
-// Inyectar analíticas de Vercel automáticamente en producción
-inject();
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import React from 'react'\nimport ReactDOM from 'react-dom/client'\nimport App from './App.jsx'\nimport './index.css'\n\nReactDOM.createRoot(document.getElementById('root')).render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>,\n)
 )
